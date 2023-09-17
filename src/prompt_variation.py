02a26402c9eee21f1651c13863ff827571e3e1cd
IgorSusmelj <igor_7779@hotmail.com>
2023-09-17 16:10:49 -0700

    Move prompt code to src

--- src/prompt_variation.py
+++ src/prompt_variation.py
@@ -1,12 +1,12 @@
 import os, subprocess
 
+
 def prompt_variation(prompt, n_variations):
-    
     # option 1
     # complete_prompt = f"""Generate {str(int(n_variations))} reformulations of the prompt delimited by 3 semicolons.
     # Keep the same style as the input, just make slight variations. Remove the semicolons from the output.
     # Format the output as a list of sentences that start with a -.
-    
+
     # ;;;{prompt};;;"""
 
     # option 2
@@ -17,23 +17,24 @@ def prompt_variation(prompt, n_variations):
     
     ;;;{prompt};;;"""
 
-
     # get the output from the model using CLI
     response = subprocess.check_output(["llm", "-m", "llama2", complete_prompt])
     response = response.decode("utf-8")
     response = response.split("\n")
-    
+
     # get the reformulations
     reformulations = []
     for line in response:
         if line.startswith("- "):
             reformulations.append(line[2:])
-    assert len(reformulations) == n_variations, f"Expected {n_variations} reformulations, but got {len(reformulations)}."
-    
+    assert (
+        len(reformulations) == n_variations
+    ), f"Expected {n_variations} reformulations, but got {len(reformulations)}."
+
     return reformulations
-    
+
+
 if __name__ == "__main__":
-    
     prompt = """A blue river with red stones."""
-    
+
     print(prompt_variation(prompt, 5))
